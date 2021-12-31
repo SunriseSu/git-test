cd192616366a53f91adb0af6ed5f73c2d76dfa07
SunriseSu <74913912@qq.com>
2021-12-31 20:55:03 +0800

    first commit

---
+++ src/main/java/com/Su/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.Su.git;
+
+/**
+ * @author Sunrise.Su
+ * @program git-test
+ * @create 2021-12-31 20:47
+ **/
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
